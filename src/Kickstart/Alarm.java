796e1d035e0de6ec646f6f731a24fc20054b9a56
mengxiang <mengxiang1@jd.com>
2018-07-27 09:00:43 +0800

    Alarm

---
+++ src/Kickstart/Alarm.java
@@ -0,0 +1,30 @@
+package Kickstart;
+
+import java.util.Scanner;
+
+/**
+ * 注意事项：
+ * Input
+
+ 2
+ 2 3 1 2 1 2 1 1 9
+ 10 10 10001 10002 10003 10004 10005 10006 89273
+
+ Output
+
+ Case #1: 52
+ Case #2: 739786670
+ */
+
+public class Alarm {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        int n = in.nextInt();
+
+        for (int k = 1; k <= n; ++k) {
+
+        }
+
+    }
+}
